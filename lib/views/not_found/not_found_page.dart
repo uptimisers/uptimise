898a16403ee1df86cc460c944fd3c7baf4750217
Jayakrishna <jayakrishnanalamolu@gmail.com>
2021-08-09 18:41:40 +0530

    Create not_found_page.dart

--- lib/views/not_found/not_found_page.dart
+++ lib/views/not_found/not_found_page.dart
@@ -5,8 +5,34 @@ class NotFoundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Page Not Found')),
+    return Scaffold(
+      body: Center(
+          child: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: const [
+          Expanded(
+            child: Image(
+              image: AssetImage('images/error404_pgNotFound.gif'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: Text(
+              'Error 404',
+              style: TextStyle(
+                  fontSize: 35, fontFamily: 'NotoMono', color: Colors.red),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(67),
+            child: Text(
+              'Page not found',
+              style: TextStyle(fontSize: 35, fontFamily: 'NotoMono'),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
