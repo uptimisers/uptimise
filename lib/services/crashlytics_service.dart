uptimisers/uptimise

commit 5e3620ef194760e92fd57368d46728fd19ace9de

Fix Crashlytics usage before Firebase.initializeApp()
* Resolves release build stuck at splash screen

Signed-off-by: Liu Jie Xu <ljx1608@outlook.com>

lib/services/crashlytics_service.dart
@@ -19,7 +19,7 @@ class DummyCrashlyticsService implements CrashlyticsService {
 }
 
 class FirebaseCrashlyticsService implements CrashlyticsService {
-  final crashlytics = FirebaseCrashlytics.instance;
+  get crashlytics => FirebaseCrashlytics.instance;
 
   @override
   Future<void> setUserId({String? id}) async {

lib/services/log_service.dart
@@ -86,5 +86,8 @@ class AppLogger {
     }
   }
 
-  Future<void> setUserId({String? id}) => _crashlytics.setUserId(id: id);
+  Future<void> setUserId({String? id}) async {
+    await firebaseAppInitializationCompleter.future;
+    _crashlytics.setUserId(id: id);
+  }
 }

commit 08aa1398ef0d95bc5192b0ed1cc9bf1852e0c5d0

Fix code issues introduced in 5e3620ef194760e92fd57368d46728fd19ace9de
Signed-off-by: Liu Jie Xu <ljx1608@outlook.com>

lib/services/crashlytics_service.dart
@@ -19,7 +19,7 @@ class DummyCrashlyticsService implements CrashlyticsService {
 }
 
 class FirebaseCrashlyticsService implements CrashlyticsService {
-  get crashlytics => FirebaseCrashlytics.instance;
+  FirebaseCrashlytics get crashlytics => FirebaseCrashlytics.instance;
 
   @override
   Future<void> setUserId({String? id}) async {

lib/services/log_service.dart
@@ -88,6 +88,6 @@ class AppLogger {
 
   Future<void> setUserId({String? id}) async {
     await firebaseAppInitializationCompleter.future;
-    _crashlytics.setUserId(id: id);
+    await _crashlytics.setUserId(id: id);
   }
 }