214af68f32bcaf080f3052579c87cdff6806af2d
Liu Jie Xu <ljx1608@outlook.com>
2021-08-10 11:07:44 +0800

    Add Google Classroom API
    Signed-off-by: Liu Jie Xu <ljx1608@outlook.com>

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,7 @@
+import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/classroom/v1.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final authProvider = Provider<AuthService>((ref) => AuthService());
@@ -13,12 +15,20 @@ class SignInAbortedException implements Exception {
 
 class AuthService {
   final auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn(
+    scopes: <String>[
+      ClassroomApi.classroomCoursesReadonlyScope,
+      ClassroomApi.classroomCourseworkMeReadonlyScope,
+      ClassroomApi.classroomAnnouncementsReadonlyScope
+    ],
+  );
 
   User? get user => auth.currentUser;
+  ClassroomApi? classroomApi;
 
   /// Might throw [SignInAbortedException] or [FirebaseAuthException].
   Future<void> signIn() async {
-    final user = await GoogleSignIn().signIn(); // triggers auth flow
+    final user = await _googleSignIn.signIn(); // triggers auth flow
     if (user == null) {
       throw const SignInAbortedException();
     }
@@ -31,7 +41,14 @@ class AuthService {
     await FirebaseAuth.instance.signInWithCredential(creds);
   }
 
+  Future<void> classroomInit() async {
+    if (await _googleSignIn.signInSilently() != null) {
+      classroomApi = ClassroomApi((await _googleSignIn.authenticatedClient())!);
+    }
+  }
+
   Future<void> signOut() async {
+    await _googleSignIn.signOut();
     await FirebaseAuth.instance.signOut();
   }
 }
